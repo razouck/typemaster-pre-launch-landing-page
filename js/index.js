64adb0072dc4c4294e1954a7a10f7e886dc5b261
razouck <razouck@users.noreply.github.com>
2024-09-28 12:18:31 -0300

    Fix animations

--- js/index.js
+++ js/index.js
@@ -20,7 +20,7 @@ for ( let i = 0; i < elements.length; i++ )
 {
 	const element = elements[ i ];
 
-	element.style.setProperty( "--play-state", "running" );
+	element.style.setProperty( "--play-state", "paused" );
 
 	observer.observe( element );
 }
